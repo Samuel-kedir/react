8c43d1235dcdb54b674f6cc3ff3419534ffcf98c
samuel kedir <samuel.datros@gmail.com>
2021-11-03 21:40:39 +0300

    commited

--- src/Home.js
+++ src/Home.js
@@ -10,11 +10,23 @@ const Home = () => {
 //         console.log("hello "+ name);
 //    }
 
+    const [name , setName] = useState('mario');
+    const [age , setAge] = useState(21);
+    const handleClick = ()=>{
+        setName('Taniya');
+    }
+        
+    const increase = ()=>{
+       setAge(age+1);
+    }
+
 
     return ( 
         <div  className="home">
             <h1>{title}</h1>
-            
+            <p>{name}  is {age} years old</p>
+            <button onClick={handleClick}>click</button>
+            <button onClick={increase}>increase</button>
             {/* <button onClick={handleClick}>Click me</button> */}
             {/* <button onClick={()=>handleClickAgain('sam')}>Click me again</button> */}
         </div>
